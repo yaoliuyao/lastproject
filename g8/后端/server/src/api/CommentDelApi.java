2ec084e9a0b293423f95b1df3e408fab5b918afa
zt-zmp <66453863+zt-zmp@users.noreply.github.com>
2021-01-18 14:50:50 +0800

    新加的一些有关评论的代码 (#18)

--- g8/后端/server/src/api/CommentDelApi.java
+++ g8/后端/server/src/api/CommentDelApi.java
@@ -1,4 +1,25 @@
 package api;
 
-public class CommentDelApi {
+import dao.CommentDao;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet("/comment/del")
+public class CommentDelApi extends HttpServlet {
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        int cid = Integer.parseInt(req.getParameter("id"));
+
+        try {
+            new CommentDao().delComment(cid);
+        } catch (Exception e) {
+            e.printStackTrace();
+            throw new RuntimeException("删除出错了！！！！");
+        }
+    }
 }
